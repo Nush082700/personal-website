355cb11910af372b4665ac800d4ec4c980b4a21b
Anusha Choudhury <ac978@cornell.edu>
2022-05-16 15:19:27 -0400

    dhbst:

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/anushachoudhury/Desktop/Main/Software/personal-website/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/anushachoudhury/Desktop/Main/Software/personal-website/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/anushachoudhury/Desktop/Main/Software/personal-website/src/pages/index.js"))
 }
